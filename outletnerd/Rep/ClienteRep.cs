841036173055776c09ba76af3ce3fd52ac3c4d37
Joorb <joorb411@gmail.com>
2025-09-30 10:44:54 -0300

    Criando o Cliente Repositório e interface

--- outletnerd/Rep/ClienteRep.cs
+++ outletnerd/Rep/ClienteRep.cs
@@ -1,9 +1,40 @@
-﻿using Dapper;
+﻿
 using MySql.Data.MySqlClient;
 using outletnerd.Models;
+using outletnerd.Rep.Interfaces;
+using System.Data;
+
 namespace outletnerd.Rep
 {
-    public class ClienteRep
-    {
+    public class ClienteRep : InCliente
+        {
+            private readonly string _connectionString;
+
+            public ClienteRep(IConfiguration conf)
+            {
+                _connectionString = conf.GetConnectionString("DefaultConnection");
+
+            }
+            public void CadastrarCliente(Cliente cliente)
+            {
+            using (var conexao = new MySqlConnection(_connectionString))
+            {
+                conexao.Open();
+                MySqlCommand comand = new MySqlCommand("insert into Cliente(Email, Nome, Senha, Telefone) values (@Email, @Nome, @Senha, @Telefone)", conexao);
+
+                comand.Parameters.Add("@Email", MySqlDbType.VarChar).Value = cliente.Email;
+                comand.Parameters.Add("@Nome", MySqlDbType.VarChar).Value = cliente.Name;
+                comand.Parameters.Add("@Senha", MySqlDbType.VarChar).Value = cliente.Senha;
+                comand.Parameters.Add("@Telefone", MySqlDbType.Int32).Value = cliente.Telefone;
+
+                comand.ExecuteNonQuery();
+                conexao.Close();
+            }
     }
-}
+     public IEnumerable<Cliente> ObterAluno()
+        {
+          
+        }
+
+        }
+    }
