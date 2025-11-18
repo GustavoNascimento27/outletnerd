303dcb3c3ebd079d1e9e34ba24008317a62dcd46
kaka <kakas13.souza@gmail.com>
2025-11-17 22:53:16 -0300

    adição do login, carrinho e tela inicial

--- outletnerd/Controllers/CompraController.cs
+++ outletnerd/Controllers/CompraController.cs
@@ -1,15 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using outletnerd.Models;
+using outletnerd.Rep;
 
 namespace outletnerd.Controllers
 {
     public class CompraController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-        
-    }
-    
+        private readonly ILogger<CompraController> _logger;
+        private readonly ProdutoRep _produtoRep;
 
+        public CompraController(ILogger<CompraController> logger, ProdutoRep produtoRep)
+        {
+            _logger = logger;
+            _produtoRep = produtoRep;
+        }
+
+        // /Compra/Index/5
+        [Route("Compra/{id:int}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            var produto = await _produtoRep.ProdutoPorId(id);
+            return View(produto);
+        }
+
+
+    }
 }
